a585aa49503fc4fa620fea1ce8374eeb4b20e53e
Minh Tri Nguyen <kevinvn.1709@gmail.com>
2018-08-28 14:21:57 +0700

    AHC-215: Create entities Customer, Address (#3)

--- src/entities/Customer.ts
+++ src/entities/Customer.ts
@@ -1,9 +1,23 @@
-/*
- * @Author: Minh Tri Nguyen 
- * @Date: 2018-08-23 15:03:41 
- * @Last Modified by: Minh Tri Nguyen
- * @Last Modified time: 2018-08-23 16:13:09
+/**
+ * Enum Salutation
+ *
+ * @export
+ * @enum {number}
  */
+export enum Salutation {
+  mr,
+  ms,
+  unknown
+}
+
+export type Address = {
+  name: string
+  street: string
+  state: string
+  zip: string
+  city: string
+  country: string
+}
 
 /**
  * @class CustomerBuilder
@@ -26,21 +40,89 @@ class CustomerBuilder {
   private _lastName: string
 
   /**
+   * Salutation property
+   *
+   * @private
+   * @type {string}
+   */
+  private _salutation: Salutation
+
+  /**
+   * Customer Id property
+   *
+   * @private
+   * @type {string}
+   */
+  private _customerId: string
+
+  /**
+   * Birthday property
+   *
+   * @private
+   * @type {Date}
+   */
+  private _birthDate: string
+
+  /**
+   * Email property
+   *
+   * @private
+   * @type {string}
+   */
+  private _email: string
+
+  /**
+   * Phone property
+   *
+   * @private
+   * @type {string}
+   */
+  private _phone: string
+
+  /**
+   * Mobile property
+   *
+   * @private
+   * @type {string}
+   */
+  private _mobile: string
+
+  /**
+   * Address property
+   *
+   * @private
+   * @type {Address}
+   */
+  private _address: Address
+
+  /**
    * Creates an instance of CustomerBuilder.
-   * @memberof CustomerBuilder
    */
   constructor() {
     this._firstName = ''
     this._lastName = ''
+    this._salutation = Salutation.mr
+    this._customerId = ''
+    this._birthDate = ''
+    this._email = ''
+    this._phone = ''
+    this._mobile = ''
+    this._address = {
+      name: '',
+      street: '',
+      state: '',
+      zip: '',
+      city: '',
+      country: ''
+    }
   }
 
   /**
    * Get first name property
    *
-   * @readonly
    * @type {string}
    */
-  get firstName(): string {
+  public getFirstName(): string {
     return this._firstName
   }
 
@@ -50,7 +132,7 @@ class CustomerBuilder {
    * @param {string} value
    * @returns {CustomerBuilder}
    */
-  setFirstName(value: string): CustomerBuilder {
+  public setFirstName(value: string): CustomerBuilder {
     this._firstName = value
     return this
   }
@@ -58,10 +140,9 @@ class CustomerBuilder {
   /**
    * Get last name property
    *
-   * @readonly
    * @type {string}
    */
-  get lastName(): string {
+  public getLastName(): string {
     return this._lastName
   }
 
@@ -71,8 +152,148 @@ class CustomerBuilder {
    * @param {string} value
    * @returns {CustomerBuilder}
    */
-  setLastName(value: string): CustomerBuilder {
+  public setLastName(value: string): CustomerBuilder {
     this._lastName = value
+    return this
+  }
+
+  /**
+   * Get Salutation
+   *
+   * @type {Salutation}
+   */
+  public getSalutation(): Salutation {
+    return this._salutation
+  }
+
+  /**
+   * Set Salutation
+   *
+   * @param {Salutation} value
+   * @returns {CustomerBuilder}
+   */
+  public setSalutation(value: Salutation): CustomerBuilder {
+    this._salutation = value
+    return this
+  }
+
+  /**
+   * Get cusomter Id
+   *
+   * @type {string}
+   */
+  public getCustomerId(): string {
+    return this._customerId
+  }
+
+  /**
+   * Set cusomter Id
+   *
+   * @param {string} value
+   * @returns {CustomerBuilder}
+   */
+  public setCustomerId(value: string): CustomerBuilder {
+    this._customerId = value
+    return this
+  }
+
+  /**
+   * Get Birthday
+   *
+   * @type {string}
+   */
+  public getBirthDate(): string {
+    return this._birthDate
+  }
+
+  /**
+   * Set Birthday
+   *
+   * @param {string} value
+   * @returns {CustomerBuilder}
+   */
+  public setBirthDate(value: string): CustomerBuilder {
+    this._birthDate = value
+    return this
+  }
+
+  /**
+   * Get Email
+   *
+   * @type {string}
+   */
+  public getEmail(): string {
+    return this._email
+  }
+
+  /**
+   * Set Email
+   *
+   * @param {string} value
+   * @returns {CustomerBuilder}
+   */
+  public setEmail(value: string): CustomerBuilder {
+    this._email = value
+    return this
+  }
+
+  /**
+   * Get Phone
+   *
+   * @type {string}
+   */
+  public getPhone(): string {
+    return this._phone
+  }
+
+  /**
+   * Set Phone
+   *
+   * @param {string} value
+   * @returns {CustomerBuilder}
+   */
+  public setPhone(value: string): CustomerBuilder {
+    this._phone = value
+    return this
+  }
+
+  /**
+   * Get Mobile
+   *
+   * @type {string}
+   */
+  public getMobile(): string {
+    return this._mobile
+  }
+
+  /**
+   * Set Mobile
+   *
+   * @param {string} value
+   * @returns {CustomerBuilder}
+   */
+  public setMobile(value: string): CustomerBuilder {
+    this._mobile = value
+    return this
+  }
+
+  /**
+   * Get Address
+   *
+   * @type {Address}
+   */
+  public getAddress(): Address {
+    return this._address
+  }
+
+  /**
+   * Set Address
+   *
+   * @param {Address} value
+   * @returns {CustomerBuilder}
+   */
+  public setAddress(value: Address): CustomerBuilder {
+    this._address = value
     return this
   }
 
@@ -94,34 +315,109 @@ class CustomerBuilder {
 class Customer {
   private _firstName: string
   private _lastName: string
+  private _salutation: Salutation
+  private _customerId: string
+  private _birthdDate: string
+  private _email: string
+  private _phone: string
+  private _mobile: string
+  private _address: Address
 
   /**
    * Creates an instance of Customer.
    * @param {CustomerBuilder} builder
    */
   constructor(builder: CustomerBuilder) {
-    this._firstName = builder.firstName
-    this._lastName = builder.lastName
+    this._firstName = builder.getFirstName()
+    this._lastName = builder.getLastName()
+    this._salutation = builder.getSalutation()
+    this._customerId = builder.getCustomerId()
+    this._birthdDate = builder.getBirthDate()
+    this._email = builder.getEmail()
+    this._phone = builder.getPhone()
+    this._mobile = builder.getMobile()
+    this._address = builder.getAddress()
   }
 
   /**
    * Get first name property
    *
-   * @readonly
    * @type {string}
    */
-  get firstName(): string {
+  public getFirstName(): string {
     return this._firstName
   }
 
   /**
    * Get last name property
    *
-   * @readonly
    * @type {string}
    */
-  get lastName(): string {
+  public getLastName(): string {
     return this._lastName
+  }
+
+  /**
+   * Get salutation
+   *
+   * @type {Salutation}
+   */
+  public getSalutation(): Salutation {
+    return this._salutation
+  }
+
+  /**
+   * Get customer Id
+   *
+   * @type {string}
+   */
+  public getCustomerId(): string {
+    return this._customerId
+  }
+
+  /**
+   * Get birthdate
+   *
+   * @type {Date}
+   */
+  public getBirthDate(): string {
+    return this._birthdDate
+  }
+
+  /**
+   * Get email
+   *
+   * @type {string}
+   */
+  public getEmail(): string {
+    return this._email
+  }
+
+  /**
+   * Get phone
+   *
+   * @type {string}
+   */
+  public getPhone(): string {
+    return this._phone
+  }
+
+  /**
+   * Get mobile
+   *
+   * @type {string}
+   */
+  public getMobile(): string {
+    return this._mobile
+  }
+
+  /**
+   * Get address
+   *
+   * @type {Address}
+   */
+  public getAddress(): Address {
+    return this._address
   }
 }
 
--- test/heidelpay.test.ts
+++ test/heidelpay.test.ts
@@ -1,9 +1,3 @@
-/*
- * @Author: Minh Tri Nguyen 
- * @Date: 2018-08-23 16:36:48 
- * @Last Modified by:   Minh Tri Nguyen 
- * @Last Modified time: 2018-08-23 16:36:48 
- */
 import Heidelpay from '../src'
 
 describe('Initial test', () => {
