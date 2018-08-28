585d2118fdcfbacc265ba06961b0eb28714fcf79
Tuan Nguyen <contact@tuannat.me>
2018-08-28 17:12:17 +0700

    fix type request adapter

--- src/adapters/IRequestAdapter.ts
+++ src/adapters/IRequestAdapter.ts
@@ -1,7 +1,7 @@
 import { Config } from '../Config'
 
 export interface IRequestAdapter {
-  get: any
-  post: any
-  put: any
+  get(payload: string): Promise<Response>
+  post(payload: string): Promise<Response>
+  put(payload: string): Promise<Response>
 }
