618f3eeb703fa8550ff2c3f5c8761b2daa0ec0ea
Minh Tri Nguyen <kevinvn.1709@gmail.com>
2019-01-10 14:25:05 +0700

    AHC-769: [NodeJS] Add EPS into NodeJS SDK (#35)

---
+++ test/payments/types/Eps.test.ts
@@ -0,0 +1,38 @@
+import Heidelpay from '../../../src/Heidelpay'
+import * as TestHelper from '../../helpers/TestHelper'
+import EPS from '../../../src/payments/types/Eps'
+
+describe('Payment Type Eps Test', () => {
+  let heidelpay: Heidelpay
+
+  const getEps = () => {
+    return new EPS()
+  }
+
+  beforeAll(() => {
+    jest.setTimeout(TestHelper.getTimeout())
+    heidelpay = TestHelper.createHeidelpayInstance()
+  })
+
+  it('Test Create Eps payment type', async () => {
+    const eps: EPS = await heidelpay.createPaymentType(getEps()) as EPS
+
+    expect(eps.getId()).toBeDefined()
+    expect(eps.getPayload()).toBeDefined()
+  })
+
+  it('Test Create Eps payment type with bic', async () => {
+    const eps: EPS = await heidelpay.createPaymentType(getEps()) as EPS
+    eps.setBic("COBADEFFXX")
+
+    expect(eps.getId()).toBeDefined()
+    expect(eps.getPayload()).toBeDefined()
+  })
+
+  it('Test Fetch Eps payment type', async () => {
+    const eps: EPS = await heidelpay.createPaymentType(getEps()) as EPS
+    const fetchEps: EPS = await heidelpay.fetchPaymentType(eps.getId()) as EPS
+
+    expect(fetchEps.getId()).toEqual(eps.getId())
+  })
+})
