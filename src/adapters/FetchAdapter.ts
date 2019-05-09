8ccccb4c2c3c2a92d3a585ba33a3cda0051cb4ab
Minh Tri Nguyen <kevinvn.1709@gmail.com>
2019-05-09 17:44:29 +0700

    SDK-62: Add SDK information to every request (#45)

--- src/adapters/FetchAdapter.ts
+++ src/adapters/FetchAdapter.ts
@@ -1,4 +1,5 @@
 const Base64 = require('js-base64').Base64
+const { version } = require('../../package.json')
 require('es6-promise').polyfill()
 require('isomorphic-fetch')
 import Environment from '../configs/Environment'
@@ -105,7 +106,9 @@ export class FetchAdapter {
 
       fetch(requestUrl, {
         headers: {
-          Authorization: `Basic ${basicAuthValue}`,
+          'SDK-TYPE': 'HeidelpayNodeJS',
+          'SDK-VERSION': version,
+          'Authorization': `Basic ${basicAuthValue}`,
           'Accept-Language': this.locale || 'en_US',
           'Content-Type': 'application/json'
         },
