4c77ae4e784b6d03c77f940cf0a56b37e0ca6cf3
Minh Tri Nguyen <kevinvn.1709@gmail.com>
2020-06-18 16:37:30 +0700

    JSSDK-1088: Introduce a invoice-secured component (#89)

---
+++ src/payments/types/InvoiceSecured.ts
@@ -0,0 +1,49 @@
+import * as apiURL from '../../configs/ApiUrls'
+import AbstractPaymentType from './AbstractPaymentType'
+import PaymentType from './PaymentType'
+
+export default class InvoiceSecured extends AbstractPaymentType implements PaymentType {
+  private _invoiceId: string
+  /**
+   * Get url end point
+   *
+   * @returns {string}
+   */
+  public getTypeUrl(): string {
+    return apiURL.URL_TYPE_INVOICE_SECURED
+  }
+
+  
+  /**
+   * Set Invoice Id
+   *
+   * @param {string} invoiceId
+   */
+  public setInvoiceId(invoiceId: string): void {
+    this._invoiceId = invoiceId
+  }
+
+  /**
+   * Get Invoice Id
+   *
+   * @returns {string}
+   */
+  public getInvoiceId(): string {
+    return this._invoiceId
+  }
+
+  /**
+   * Get Payload
+   *
+   * @returns
+   */
+  public getPayload() {
+    if (typeof this.getInvoiceId() === 'undefined') {
+      return {}
+    }
+    
+    return {
+      invoiceId: this._invoiceId
+    }
+  }
+}
